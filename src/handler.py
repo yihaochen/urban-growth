8f93fa3c4edef5fd2946e7198a74aca7a7f778e7
Yi-Hao Chen <yihaochentw@gmail.com>
2019-10-07 23:32:10 -0700

    Handle error when geojson does not overlap with raster image
     - Use 'regions' table for each geojson entry
     - Decrease the number of scenes if error occurs in a scene

--- src/handler.py
+++ src/handler.py
@@ -40,18 +40,26 @@ def calc_urban_score(event, context):
 
         query_id = args['query_id']
         product_id = args['product_id']
+        geojson_s3_key = args['geojson_s3_key']
         geojson = get_geojson(args)
 
-        # Short-wave Infrared - Band 6
-        image_swir = get_image(product_id, 'B6', geojson)
-        # Near Infrared - Band 5
-        image_nir =  get_image(product_id, 'B5', geojson)
+        try:
+            # Short-wave Infrared - Band 6
+            image_swir = get_image(product_id, 'B6', geojson)
+            # Near Infrared - Band 5
+            image_nir =  get_image(product_id, 'B5', geojson)
+        except ValueError:
+            # mask region does not overlap with raster image
+            print('Encountered error in %s, removing scenes...' % product_id)
+            db_response = decrease_counter(geojson_s3_key)
+            return db_response
 
         # Calculate the Normalized Difference Built-up Index
         ndbi = (image_swir-image_nir)/(image_swir+image_nir)
 
         # Calculate the urban score
-        urban_score = np.nan_to_num(ndbi).sum() + np.count_nonzero(image_swir)
+        urban_score = np.nan_to_num(ndbi, posinf=0, neginf=0).sum() / np.count_nonzero(image_swir)
+        urban_score = np.nan_to_num(urban_score)
 
         date = get_landsat_date(product_id)
 
@@ -70,6 +78,7 @@ def calc_urban_score(event, context):
 
         outputs.append(db_item)
 
+        print('Updating DB:', db_item)
         db_response = update_db(db_item)
 
     response = prep_response(outputs)
@@ -79,6 +88,11 @@ def calc_urban_score(event, context):
 
 def get_scenes_send_queues(event, context):
     '''
+
+    Input: args or args in the body of event
+    args is a dictionary containing
+        'geojson_s3_key': key (or the path) to geojson file on S3
+        'cloud_cover_range': (min, max) cloud coverage
     '''
     args = parse_args(event)
     geojson_s3_key = args['geojson_s3_key']
@@ -88,18 +102,30 @@ def get_scenes_send_queues(event, context):
     if 'cloud_cover_range' in args.keys():
         cloud_cover_range = args['cloud_cover_range']
     else:
-        cloud_cover_range = (0, 5)
+        cloud_cover_range = (0, 10)
     items = search_scenes(bbox, cloud_cover=cloud_cover_range)
     print('Found %3i scenes' % len(items))
 
-    responses = []
-
     for item in items:
+        # Send job to SQS
         job = {"query_id": query_id,
                "product_id": item.properties["landsat:product_id"],
                "geojson_s3_key": geojson_s3_key
               }
+        print('Sending message to SQS:', job)
         response = send_queue(job)
-        responses.append(response)
 
-    return responses
+    # Update the regions table
+    db_item = {"geojson_s3_key": {"S": str(geojson_s3_key)},
+               "query_id": {"S": str(query_id)},
+               "number_of_scenes": {"N": str(len(items))}
+              }
+    db_response = update_db(db_item, table_name='regions')
+
+    output = {"query_id": query_id,
+              "geojson_s3_key": geojson_s3_key,
+              "number_of_scenes": len(items)
+             }
+    response = prep_response(output)
+
+    return response
